3ced7984b09961f1987c61b1f1c2ff1611987ffe
AYSHATHFAHMIDA <ayshafahmi26@gmail.com>
2023-01-09 20:03:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home: MYAPP(),debugShowCheckedModeBanner: false,));
+}
+class MYAPP extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+
+}
+
